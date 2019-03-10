a870f51d0087470cbb726e608f93d885d6871088
EndAroundCarry <gantubogdan@gmail.com>
2019-03-10 15:59:45 +0200

    updated the compile file

--- Team1_Farmacie/compile.bat
+++ Team1_Farmacie/compile.bat
@@ -1,5 +1,10 @@
 start ant
-timeout 3 
+timeout 5
+start xcopy /y/i %CD% bin
 cd bin
-start jar cvf Farmacie.jar *
+start jar cf Farmacie.jar *
+::start jar cvf Farmacie.jar *.class
+::move Farmacie.jar C:\Users\gantu\git\Pharmacy\Team1_Farmacie
+::for /r %i in ("Team1_Farmacie\*.*") do move %i %~pi..
+
 
